2c0d35224b1d5c73346312544fd78404a76eeaff
guolyuying <149437119+guolyuying@users.noreply.github.com>
2023-10-30 18:23:11 -0400

    Add files via upload

---
+++ reading files.py
@@ -0,0 +1,30 @@
+hearts = open("hearts.txt", "r")
+
+#print(hearts.readable())
+
+#print(hearts.read())
+
+#print(hearts.readlines())
+#print(hearts.readlines()[0])
+
+#for ting in hearts.readlines():
+#    print(ting)
+
+print(hearts.readline())
+print(hearts.readline())
+print(hearts.readline())
+print(hearts.readline())
+print(hearts.readline())
+print(hearts.readline())
+print(hearts.readline())
+print(hearts.readline())
+print(hearts.readline())
+print(hearts.readline())
+
+
+
+
+
+
+
+hearts.close()
---
+++ writing to files.py
@@ -0,0 +1,17 @@
+#hearts = open("hearts2.txt", "a")
+#hearts.write("- is me hubby")
+#hearts.close()
+
+#hearts = open("hearts2.txt", "a")
+#hearts.write("\n- is me hubby")
+#hearts.close()
+
+#hearts = open("hearts2.txt", "w")
+#hearts.write("- is me hubby")
+#hearts.close()
+
+hearts = open("hearts3.txt", "w")
+hearts.write("- is me hubby")
+hearts.close()
+
+
