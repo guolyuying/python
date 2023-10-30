08aefaaa94fb1a4e9e3e1f1218b48c54b55d9f92
guolyuying <149437119+guolyuying@users.noreply.github.com>
2023-10-30 18:15:55 -0400

    Add files via upload

---
+++ number.py
@@ -0,0 +1,28 @@
+print(2)
+print(2.01)
+print(-2)
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3/4)
+print(3*4+5)
+print(3*(4+5))
+print(10%3)
+
+my_num = 5
+print(my_num)
+
+print(str(5))
+print(str(5) + "my fav num")
+
+print(abs(-5))
+print(pow(2,3))
+print(max(2,3,4))
+print(min(2,3,4))
+print(round(3.2))
+
+from math import *
+print(floor(3.2))
+print(ceil(3.2))
+print(sqrt(36))
---
+++ string.py
@@ -0,0 +1,17 @@
+print("Giraffe Academy")
+print("Giraffe\nAcademy")
+print("Giraffe\"Academy")
+print("Giraffe\Academy")
+
+phrase = "Giraffe Academy"
+print(phrase)
+print(phrase + " is cool")
+
+print(phrase.upper())
+print(phrase.lower())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("G"))
+print(phrase.replace("Giraffe", "Elephant"))
